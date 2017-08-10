6a5698eda4f1b79a57551b2cfeda4b001e1778f2
NikitaD111 <league@WTS9.attlocal.net>
2017-08-09 20:27:23 -0700

    Day 3 Workshop

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -3,12 +3,10 @@ package day3;
 import javax.swing.JOptionPane;
 
 /**
- * Teacher's Note: 
- * Have the kids play with the Speak & Spell. 
- * The first Speak & Spell was introduced at the summer Consumer Electronics Show in June 1978, making it 
- * one of the earliest handheld electronic devices with a visual display to use interchangeable game cartridges. 
- * Discuss with students how you would make this program. 
- * Allow them to code it themselves, or use this recipe.
+ * Teacher's Note: Have the kids play with the Speak & Spell. The first Speak & Spell was introduced at the summer
+ * Consumer Electronics Show in June 1978, making it one of the earliest handheld electronic devices with a visual
+ * display to use interchangeable game cartridges. Discuss with students how you would make this program. Allow them to
+ * code it themselves, or use this recipe.
  **/
 
 public class SpeakAndSpell {
@@ -16,14 +14,26 @@ public class SpeakAndSpell {
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
 
+		speak("Please spell atreeisalife");
+		String name;
 		// 2. Catch the user's answer in a String
+		name = JOptionPane.showInputDialog("Type the word");
 
 		// 3. If the user spelled the word correctly, speak "correct"
-
+		if (name.equals("atreeisalife")) {
+			speak("correct");
+		}
 		// 4. Otherwise say "wrong"
-
+		else {
+			speak("wrong");
+		}
 		// 5. repeat the process for other words
-		
+
+	}
+
+	private static void showInputDialog(String string) {
+		// TODO Auto-generated method stub
+
 	}
 
 	static void speak(String words) {
@@ -35,5 +45,3 @@ public class SpeakAndSpell {
 	}
 
 }
-
-
